45908fd7fc7abddb27724fd42568ece6b70ec983
f4shn <githubenko@proton.me>
2024-12-07 15:29:35 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/loader.lua", true))("discord.gg/immortalhub")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/loader.lua", true))("discord.gg/PwTPex2g6T")
