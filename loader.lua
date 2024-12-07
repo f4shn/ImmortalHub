f014a525d20e0e60e658bbbfb3866c24ef720ac9
f4shn <githubenko@proton.me>
2024-12-07 13:26:41 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,11 +1,11 @@
 repeat task.wait() until game:IsLoaded()
 
 local Games = {
-    [66654135] = ""
+    [66654135] = "https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/mm2.lua"
 }
 
 for i, v in pairs(Games) do
-	if i == game.GameId then
+	if i == game.GameId or i == game.PlaceId then
 		loadstring(game:HttpGet(v))()
 	end
 end
