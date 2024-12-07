c4ebe735ac0311ab575e5cd560dad37de8282425
f4shn <githubenko@proton.me>
2024-12-07 13:24:23 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,11 @@
+repeat task.wait() until game:IsLoaded()
 
+local Games = {
+    [66654135] = ""
+}
+
+for i, v in pairs(Games) do
+	if i == game.GameId then
+		loadstring(game:HttpGet(v))()
+	end
+end
