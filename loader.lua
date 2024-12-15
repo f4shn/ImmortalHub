5952e3dc5b8824b35850c72a3d987ebd5a120bda
f4shn <githubenko@proton.me>
2024-12-15 21:14:56 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,8 @@
 repeat task.wait() until game:IsLoaded()
 
 local Games = {
-    [66654135] = "https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/mm2.lua"
+    [66654135] = "https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/mm2.lua",
+    [5830262966] = "https://raw.githubusercontent.com/f4shn/ImmortalHub/refs/heads/main/impossiblesquidbridge.lua"
 }
 
 for i, v in pairs(Games) do
